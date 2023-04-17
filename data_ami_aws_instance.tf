4cece2499c978220fb7e408a17ce94b27f4e4610
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-04-17 19:20:56 +0300

    Update data_ami_aws_instance.tf

--- data_ami_aws_instance.tf
+++ data_ami_aws_instance.tf
@@ -1,13 +1,18 @@
 data "aws_ami" "ubuntu" {
-    filters = {
-      architecture        = "x86_64"
-      name                = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-      root-device-type    = "ebs"
-      virtualization-type = "hvm"
-    }
-    most_recent = true
-    owners      = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "VM" {
   instance_type = "t2.micro"
